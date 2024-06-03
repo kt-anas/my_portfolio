c331a5e46b12ed5654b3c2bfdbf052db27103808
anas <ktmanas2020@gmail.com>
2024-06-03 16:06:32 +0530

    more changes

---
+++ javascript.js
@@ -0,0 +1,21 @@
+// function sendMail() {
+//     var params = {
+//         name: document.getElementById("name").value,
+//         email: document.getElementById("email").value,
+//         message: document.getElementById("message").value
+//     };
+
+// const serviceID = "service_8qjv7jq";
+// const templateID = "template_1j9ylhp"
+
+// emailjs.send(serviceID, templateID,params)
+//     .then(
+//         res => {
+//             document.getElementById("name").value = "";
+//             document.getElementById("email").value = "";
+//             document.getElementById("message").value = "";
+//             console.log(res);
+//             alert("your message sent successfully");
+//         }
+//     ).catch((err) => console.log(err));
+// }
